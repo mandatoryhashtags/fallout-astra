f5793149bfdf3b5414e72d78ad463f2affda7d1a
Curtis Schrum <cjschrum@gmail.com>
2025-02-07 14:38:40 -0600

    Added Astrogation

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,11 @@
 Hooks.once('init', async function () {
-    console.log('Your Module | Initializing module');
+    console.log('Fallout Astra | Initializing module');
+
+    // Register the new Astrogation skill
+    CONFIG.FALLOUT.skills['astrogation'] = {
+        label: "FALLOUT.SkillAstrogation",
+        attribute: "int"  // Based on Intellect
+    };
 });
 
 Hooks.once('ready', async function () {
