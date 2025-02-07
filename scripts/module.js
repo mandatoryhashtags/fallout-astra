aee67e2a0f97a272175b8df490f9dbd421fa849f
Curtis Schrum <cjschrum@gmail.com>
2025-02-07 14:34:38 -0600

    Initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -1,1 +1,7 @@
- 
+Hooks.once('init', async function () {
+    console.log('Your Module | Initializing module');
+});
+
+Hooks.once('ready', async function () {
+    console.log('Your Module | Module is ready');
+}); 
